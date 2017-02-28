23631df580a1883f00170c5756d01cb5a6dec95d
Ryan Byrne <ryanbyrne@Ryans-MacBook-Pro.local>
2017-02-27 20:26:38 -0500

    order model attributes added

--- src/components/containers/Orders.js
+++ src/components/containers/Orders.js
@@ -7,12 +7,30 @@ class Orders extends Component{
   componentDidMount(){
     //this.props.getNonpackedOrders({account: this.props.user.id, packed: false})
     this.props.getNonpackedOrders({packed: false})
+
+  }
+
+  packOrder(event, id){
+    this.props.markOrderPacked({id: id, packed: true})
   }
 
   render(){
     return(
       <div>
-        ORDERS
+        <h1>ORDERS</h1>
+        <h3>To be Packed</h3>
+        {
+          (this.props.nonPackedOrders.list.length==0) ? <h3>All packed</h3> :
+          <ol>
+            {
+              this.props.nonPackedOrders.list.map((order, i) => {
+                return <li key={order.id}>{order.id + ':  PACKED:   ' + order.packed}</li>
+              })
+            }
+          </ol>
+        }
+        <br /> <br /> <br />
+        <h3>Recently Packed</h3>
         {
           (this.props.nonPackedOrders.list.length==0) ? <h3>All packed</h3> :
           <ol>
@@ -37,7 +55,8 @@ const stateToProps = (state) => {
 
 const dispatchToProps = (dispatch) => {
   return {
-    getNonpackedOrders:(params) => dispatch(actions.getNonpackedOrders(params)) 
+    getNonpackedOrders:(params) => dispatch(actions.getNonpackedOrders(params)),
+    markOrderPacked: (params) => dispatch(actions.markOrderPacked(params))
   }
 }
 
